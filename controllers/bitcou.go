bda2d0e597ae03298f617bc904d171370912e2ed
saracarolina12 <0226594@up.edu.mx>
2021-12-22 13:55:46 -0600

    'Basic-Auth-Implementation'

--- controllers/bitcou.go
+++ controllers/bitcou.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"encoding/base64"
 	"fmt"
 	"github.com/bitcou/bitcou-wrapper/bitcou"
 	wrap_err "github.com/bitcou/bitcou-wrapper/errors"
@@ -28,26 +29,32 @@ func (b *BitcouController) CreateOrder(c *gin.Context) {
 		// "data":    gin.H{"email": "data@example.com", "phone": "02020202"},
 	}
 	fmt.Println("data: ", data)
-	gin.BasicAuth(gin.Accounts{
+	// authorized:= gin.BasicAuth(gin.Accounts{
+	// 	"hestia": "A7Xm9WbUZG7cT2Au",
+	// 	// "hello": "326363246",
+	// })
+	fakeauthorized:=map[string]string{
 		"hestia": "A7Xm9WbUZG7cT2Au",
-		// "hello": "326363246",
-	})
-	// c.JSON(http.StatusOK, gin.H{"data" : data})
-	// authorized.POST("/", func(x *gin.Context) {
-		user := c.Request.Header["Authorization"]
-		fmt.Println("user: ",user[0])
-		if user[0] == "Basic aGVzdGlhOkE3WG05V2JVWkc3Y1QyQXU="{
+		"hello": "326363246",
+	}
+		user := ((c.Request.Header["Authorization"])[0])[6:len(c.Request.Header["Authorization"][0])]
+		decodedUser, err := base64.StdEncoding.DecodeString(user)
+		// fmt.Println("fakeauthorized:", fakeauthorized)
+		founded := false;
+		for a,b := range fakeauthorized {
+			s := string(a)+":"+string(b)
+			if(string(decodedUser) == s){
+				founded = true 
+			}
+		}
+		println("exists? ",founded)
+		fmt.Println("user: ", user)
+		fmt.Println("decodedUser: ",string(decodedUser))
+		if (founded == true) {
 			c.JSON(http.StatusOK, gin.H{"data": data})
 		}else{
 			c.JSON(http.StatusOK, gin.H{"data":"not authorized"})
 		}
-		// if d, ok:= data[user]; ok{
-		// 	c.JSON(http.StatusOK, gin.H{"user": user, "secret": d})
-		// } else {
-		// 	c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
-		// }
-	
-
 
 	body := c.Request.Body
 	value, err := ioutil.ReadAll(body)
